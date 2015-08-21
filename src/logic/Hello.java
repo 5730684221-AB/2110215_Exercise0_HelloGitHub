85df86bc3d55243c39c72ef54a790e1e9445a2a7
Administrator <Administrator@com30>
2015-08-21 11:26:58 +0700

    implement

--- src/logic/Hello.java
+++ src/logic/Hello.java
@@ -24,8 +24,17 @@ public class Hello {
 		do{
 			isSwap=false;
 			for (int i=0;i<array.length-1;i++){
-				
+				if (array[i]>array[i+1])
+				{
+					int temp=array[i];
+					array[i]=array[i+1];
+					array[i+1]=temp;
+					isSwap=true;
+				}	
+				}
 			}
+		while(isSwap);
+		return array;
 		}
 		return array;
 	}
